8322423f8f4f7c278fdcffc06f14507c7beb27de
Melderan <Melderan@gmail.com>
2013-04-04 15:57:14 -0500

    Modified Challenge1 to have arguments.  Will function from command line and let you set each parameter of the script

--- challenge1.py
+++ challenge1.py
@@ -7,27 +7,40 @@ Write a script that builds three 512 MB Cloud Servers that following a similar n
 # Import Libraries
 import pyrax                    # Rackspace API
 import time                     # Time lib for sleep(seconds)
+import argparse                 # Argument Parser Library
 from os.path import expanduser  # Expands '~' for user home folder
 
+# Setup our argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--credentials', help="Specify Credentials file.", type=str, default='~/.rackspace_cloud_credentials')
+parser.add_argument('-i', '--image', help="Specify image ID to build servers from.", type=str, default='Arch')
+parser.add_argument('-f', '--flavor', help="Amount of RAM to be given to the server.", type=int, default=512)
+parser.add_argument('-s', '--serverName', help="The base name that all the created servers will share.", type=str, default='Challenge1-')
+parser.add_argument('-n', '--number', help="Number of new Cloud Servers to build.", type=int, default=3)
+args = parser.parse_args()
+
 # Set Authentication File
-pyrax.set_credential_file(expanduser("~/.rackspace_cloud_credentials"))
+pyrax.set_credential_file(expanduser(args.credentials))
 # Authenticate and get Cloud Servers handle
 cs = pyrax.cloudservers
 
-# Get the Image id from Image Object for 'Arch'
-imgID = [img for img in cs.images.list() if 'Arch' in img.name][0].id
+# Check if -i or --image were used
+if args.image == 'Arch':
+    imgID = [img for img in cs.images.list() if 'Arch' in img.name][0].id
+else:
+    imgID = args.image
 
-# Get the Flavor id from Flavor Object for '512M'
-flvID = [flv for flv in cs.flavors.list() if flv.ram == 512][0].id
+# Get the Flavor id from Flavor Object for args.flavor
+flvID = [flv for flv in cs.flavors.list() if flv.ram == args.flavor][0].id
 
-# Base server name
-newServerNameBase = "Arch-Challenge1-"
+# Base server name from args
+newServerNameBase = args.serverName
 
 # Base server number
 newServerNumberBase = len([srv for srv in cs.servers.list() if newServerNameBase in srv.name])
 
 # Number of server to make
-numNewServersToMake = 3
+numNewServersToMake = args.number
 
 # Make the servers and store the objects in a list
 newServers = [cs.servers.create(newServerNameBase + str(newServerNumberBase + i), imgID, flvID) for i in xrange(numNewServersToMake)]
