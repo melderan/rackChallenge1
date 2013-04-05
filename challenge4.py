62ec3ca00090a1bfd8d19c4118aa4bc2599ad49c
Melderan <Melderan@gmail.com>
2013-04-04 23:23:49 -0500

    Working Challenge4.  Goes on the basis of true sub domain zones and validates users configuration

--- challenge4.py
+++ challenge4.py
@@ -5,4 +5,43 @@ a FQDN and IP address as arguments.
 '''
 
 
+# Import Libraries
+import time
+import argparse
+import pyrax
+from os.path import expanduser
 
+# Setup our argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('FQDN', help='Fully Qualified Domain Name (i.e. test.example.com)', type=str)
+parser.add_argument('IP', help='IP Address to associate name with (i.e. 166.57.4.122)', type=str)
+args = parser.parse_args()
+
+# Authenticate to Pyrax
+pyrax.set_credential_file(expanduser('~/.rackspace_cloud_credentials'))
+
+# Get our handle to cloud dns
+dns = pyrax.cloud_dns
+
+# Now I am going under the assumption that we will be building our nodes in a proper
+# dns tree structure.  leaf.tree.root.tld  With this assumption I am going to try
+# and place the corresponding FQDN into the proper domain container (or sub domain container)
+
+# Parse input FQDN and try and get closest leaf/root domain name
+inputFQDN = args.FQDN
+leaf = '.'.join(inputFQDN.split('.')[1:])
+
+# now that we know the leaf lests
+domains = [dom for dom in dns.get_domain_iterator() if leaf in dom.name]
+
+if len(domains) != 1:
+    print 'You do not have a properly created subdomain setup for this domain: {}'.format(leaf)
+    exit(1)
+
+# Build the record for DNS
+record = [{
+            "type":"A",
+            "name":args.FQDN,
+            "data":args.IP}]
+
+print dns.add_records(domains[0], record)
